b9a1eeae95e1c2c15db8588dd8be5fc7fcd8ba57
Lucas MOULIN <lmoulin@e2r6p7.42.fr>
2019-11-21 17:05:12 +0100

    creation img

--- miniRT.c
+++ miniRT.c
@@ -6,15 +6,15 @@
 /*   By: lmoulin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 15:25:31 by lmoulin           #+#    #+#             */
-/*   Updated: 2019/11/20 18:34:32 by lmoulin          ###   ########.fr       */
+/*   Updated: 2019/11/21 17:03:18 by lmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "inc/miniRT.h"
+#include "inc/minirt.h"
 #include "libft/libft.h"
 #include "minilibx_opengl/mlx.h"
 
-t_mlx	mlx;
+
 
 void	ft_init(t_data *data)
 {
@@ -62,9 +62,9 @@ int		main(int ac, char **av)
 
 	if (!(data = malloc(sizeof(struct s_data) * 1)))
 		return (0);
-	if (!(mlx.mlx_ptr = malloc(sizeof(void*) * 1)))
+	if (!(g_mlx.mlx_ptr = malloc(sizeof(void*) * 1)))
 		return (0);
-	if (!(mlx.win_ptr = malloc(sizeof(void*) * 1)))
+	if (!(g_mlx.win_ptr = malloc(sizeof(void*) * 1)))
 		return (0);
 	if (ft_malloc_data(data) == -1)
 		return (0);
@@ -77,12 +77,12 @@ int		main(int ac, char **av)
 //	ft_print_param(data);
 	int	x = 0, y = 0;
 
-	if ((mlx.mlx_ptr = mlx_init()) == NULL)
+	if ((g_mlx.mlx_ptr = mlx_init()) == NULL)
 		return (-1);
-	if ((mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, data->render[0], data->render[1], "miniRT")) == NULL)
+	if ((g_mlx.win_ptr = mlx_new_window(g_mlx.mlx_ptr, data->render[0], data->render[1], "miniRT")) == NULL)
 		return (-1);
-	data->img->image = ft_new_img(mlx, data);
-//	printf("%d\n", data->img->bpp);
+/*	data->img->image = */ft_new_img(g_mlx, data);
+	printf("%s\n", data->img->image);
 	while (x < data->render[1])
 	{
 		y = 0;
@@ -94,10 +94,9 @@ int		main(int ac, char **av)
 		}
 		x++;
 	}
-//	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, data->img->image, 0, 0);
-//	put_pixel_to_img(data->image, tab, ft_get_color_hexa(data->sphere->color), data);
-	mlx_key_hook(mlx.win_ptr, esc_key, 0);
-	mlx_mouse_hook(mlx.win_ptr, close_window, 0);
-	mlx_loop(mlx.mlx_ptr);
+	mlx_put_image_to_window(g_mlx.mlx_ptr, g_mlx.win_ptr, data->img->image, 0, 0);
+	mlx_key_hook(g_mlx.win_ptr, esc_key, 0);
+	mlx_mouse_hook(g_mlx.win_ptr, close_window, 0);
+	mlx_loop(g_mlx.mlx_ptr);
 	return (0);
 }
