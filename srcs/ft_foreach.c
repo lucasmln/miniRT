653e799469f1f4bf46f55d71bfd24bf6b1a32623
Lucas MOULIN <lmoulin@e2r4p2.42.fr>
2019-12-18 17:28:29 +0100

    save

--- srcs/ft_foreach.c
+++ srcs/ft_foreach.c
@@ -6,7 +6,7 @@
 /*   By: lmoulin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/17 20:18:57 by lmoulin           #+#    #+#             */
-/*   Updated: 2019/12/18 15:50:47 by lmoulin          ###   ########.fr       */
+/*   Updated: 2019/12/18 17:17:12 by lmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ double		ft_for_each_obj(t_ray ray, t_data *data, t_vect3 *p, t_vect3 *n)
 	t_vect3	tmp_p;
 	t_vect3	tmp_n;
 
+	res = 0;
 	ft_go_start_lst(data, "all obj");
 	if (data->sp->next)
 		res = ft_for_each_sp(ray, data, p, n);
@@ -26,7 +27,7 @@ double		ft_for_each_obj(t_ray ray, t_data *data, t_vect3 *p, t_vect3 *n)
 	data->obj = "sp";
 	if (data->pl->next)
 		tmp = ft_for_each_pl(ray, data, &tmp_p, &tmp_n);
-	if ((tmp > 0 && tmp < res && res > 0) || (res <= 0 && tmp > 0))
+	if (!res || (tmp > 0 && tmp < res && res > 0) || (res <= 0 && tmp > 0))
 	{
 		res = tmp;
 		*p = tmp_p;
@@ -36,7 +37,7 @@ double		ft_for_each_obj(t_ray ray, t_data *data, t_vect3 *p, t_vect3 *n)
 	}
 	if (data->tr->next)
 		tmp = ft_for_each_tr(ray, data, &tmp_p, &tmp_n);
-	if ((tmp > 0 && tmp < res && res > 0) || (res <= 0 && tmp > 0))
+	if (!res || (tmp > 0 && tmp < res && res > 0) || (res <= 0 && tmp > 0))
 	{
 		res = tmp;
 		*p = tmp_p;
@@ -46,9 +47,8 @@ double		ft_for_each_obj(t_ray ray, t_data *data, t_vect3 *p, t_vect3 *n)
 	}
 	if (data->sq->next)
 		tmp = ft_for_each_square(ray, data, &tmp_p, &tmp_n);
-	if ((tmp > 0 && tmp < res && res > 0) || (res <= 0 && tmp > 0))
+	if (!res || (tmp > 0 && tmp < res && res > 0) || (res <= 0 && tmp > 0))
 	{
-		printf("tmp = %lf\n", tmp);
 		res = tmp;
 		*p = tmp_p;
 		*n = tmp_n;
